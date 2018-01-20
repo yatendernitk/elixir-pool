c1598eea4041013370ee51c3e7c799151dbdec4e
ok <ok@Yatenders-MacBook-Pro-2.local>
2018-01-20 16:06:47 +0530

    supervisor worker implementation

--- lib/worker_supervisor.ex
+++ lib/worker_supervisor.ex
@@ -2,10 +2,10 @@ defmodule Pooly.WorkerSupervisor do
   use Supervisor
 
   def start_link({_, _, _} = mfa) do
-    Supervisor.start_link(mfa)
+    Supervisor.start_link(__MODULE__ , mfa)
   end
 
-  def init({m, f, a} = x) do
+  def init({m, f, a}) do
     worker_opts = [restart: :permanent, function: f]
     children = [worker(m, a, worker_opts)]
     opts = [strategy: :simple_one_for_one, max_restarts: 5, max_seconds: 5]
