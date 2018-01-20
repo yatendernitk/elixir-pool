75b40cf28b9ba862430c627c2f0650e0d0791b87
ok <ok@Yatenders-MacBook-Pro-2.local>
2018-01-20 14:41:48 +0530

    removing extra files from git and adding worker

--- lib/worker_supervisor.ex
+++ lib/worker_supervisor.ex
@@ -8,9 +8,7 @@ defmodule Pooly.WorkerSupervisor do
   def init({m, f, a} = x) do
     worker_opts = [restart: :permanent, function: f]
     children = [worker(m, a, worker_opts)]
-    opts = [strategy: :simple_one_for_one,
-      max_restarts: 5,
-      max_seconds: 5]
+    opts = [strategy: :simple_one_for_one, max_restarts: 5, max_seconds: 5]
     supervise(children, opts)
   end
 end
