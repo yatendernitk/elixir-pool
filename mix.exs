3c010989704735b2972978deec9c309733ea1793
ok <ok@Yatenders-MacBook-Pro-2.local>
2018-01-23 12:00:06 +0530

    pooly addition

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Pooly.Mixfile do
       app: :pooly,
       version: "0.1.0",
       elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
@@ -14,7 +14,7 @@ defmodule Pooly.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      [applications: [:logger], mod: {Pooly, []}]
     ]
   end
 
